e28717f6b9fb67c1856a8e85d88361f64c0c7f62
Darshak2122 <dodiyadp1999@gmail.com>
2025-01-16 11:05:38 +0530

    { remove comment code }

--- packages/sheradcompo/src/text-feild/Textfield.tsx
+++ packages/sheradcompo/src/text-feild/Textfield.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 
  export function TextFields() {
-  // const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   console.log("event>>>>",event)
-  // }
+  
   return (
     <div>
     <input style={{height:"40px",width:"300px"}} placeholder='Jay hind Jay bharat' type='text' />
